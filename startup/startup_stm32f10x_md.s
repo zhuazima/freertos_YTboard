cdce0d0a6d7eb7c06f37b93054a501e59a8efcde
zhuazima <1025854946@qq.com>
2022-03-18 20:34:48 +0800

    change file startup

--- startup/startup_stm32f10x_md.s
+++ startup/startup_stm32f10x_md.s
@@ -69,11 +69,11 @@ __Vectors       DCD     __initial_sp               ; Top of Stack
                 DCD     0                          ; Reserved
                 DCD     0                          ; Reserved
                 DCD     0                          ; Reserved
-                DCD     SVC_Handler                ; SVCall Handler
+                DCD     vPortSVCHandler                ; SVCall Handler
                 DCD     DebugMon_Handler           ; Debug Monitor Handler
                 DCD     0                          ; Reserved
-                DCD     PendSV_Handler             ; PendSV Handler
-                DCD     SysTick_Handler            ; SysTick Handler
+                DCD     xPortPendSVHandler             ; PendSV Handler
+                DCD     xPortSysTickHandler            ; SysTick Handler
 
                 ; External Interrupts
                 DCD     WWDG_IRQHandler            ; Window Watchdog
